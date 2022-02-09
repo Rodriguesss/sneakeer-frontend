91173754834581fb1e4feea9740765079719f3e0
rodriguesss <viniciusrdefreitas@gmail.com>
2022-02-09 16:29:39 -0300

    adding dotenv and settings for vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 const isProd = process.env.NODE_ENV === 'production'
+
+dotenv.config({
+  path: isProd ? '.env.production' : '.env'
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5000,
+    port: process.env.PORT,
     https: isProd,
     cors: true
   },
